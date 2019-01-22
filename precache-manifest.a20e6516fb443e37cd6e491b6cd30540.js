30c11669ac96ed979b266509cbaf501c262ebe46
Intan Puspitasari <intanpusss@gmail.com>
2019-01-22 21:38:52 +0700

    Updates

---
+++ precache-manifest.a20e6516fb443e37cd6e491b6cd30540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9e804ef9bab5a60d2ca6b646a04f250",
+    "url": "/about/static/media/book.f9e804ef.jpg"
+  },
+  {
+    "revision": "2eb993563b473bafd9dd",
+    "url": "/about/static/js/runtime~main.2eb99356.js"
+  },
+  {
+    "revision": "6442cc1511936c91dbb3",
+    "url": "/about/static/js/main.6442cc15.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/about/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "6442cc1511936c91dbb3",
+    "url": "/about/static/css/main.ab784d7d.chunk.css"
+  },
+  {
+    "revision": "57bd04df17a73ad704e11d74d46066b5",
+    "url": "/about/index.html"
+  }
+];
